3c40fb1f5b29cbb0994173bd755cdc2f492330bc
Valov Ivan <pechenkooo3@gmail.com>
2022-11-27 17:59:46 +0200

    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    console.log(data);
+  }, 1000)
+);
+
+player.on('timeupdate', function (data) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+});
+
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
+// seconds = currentTime;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,43 @@
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+const userData = {
+  email: '',
+  message: '',
+};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  message: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.message.addEventListener('input', throttle(onSetData, 500));
+refs.email.addEventListener('input', throttle(onSetData, 500));
+
+onGetData();
+
+function onSetData(evt) {
+  userData.email = refs.email.value;
+  userData.message = refs.message.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(userData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(`e-mail: ${refs.email.value}, message: ${refs.message.value}`);
+  evt.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function onGetData() {
+  const saveData = localStorage.getItem(LOCALSTORAGE_KEY);
+  const parseData = JSON.parse(saveData);
+
+  if (parseData) {
+    refs.email.value = parseData.email || '';
+    refs.message.value = parseData.message || '';
+  }
+}
